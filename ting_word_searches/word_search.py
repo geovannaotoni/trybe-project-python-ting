1e3805cd7a846ab145474d155ef0e672447a0718
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-31 14:54:57 -0300

    feat08: search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,7 +1,18 @@
 from ting_file_management.queue import Queue
 
 
-def exists_word(word: str, instance: Queue):
+def line_ocurrences_report(line: int, content: str, show_cont: bool = False):
+    if show_cont:
+        return {
+            "linha": line + 1,
+            "conteudo": content
+        }
+    return {
+        "linha": line + 1
+    }
+
+
+def exists_word(word: str, instance: Queue, show_cont: bool = False):
     occurrences_files = list()
 
     # verifica dentro de cada arquivo se a palavra existe
@@ -13,7 +24,9 @@ def exists_word(word: str, instance: Queue):
         # verifica dentro de cada linha do arquivo se a palavra existe
         for line in range(len(file_data)):
             if word.casefold() in file_data[line].casefold():
-                ocurrences_lines.append({"linha": line + 1})
+                ocurrences_lines.append(
+                    line_ocurrences_report(line, file_data[line], show_cont)
+                )
 
         if len(ocurrences_lines) > 0:
             occurrences_files.append({
@@ -25,5 +38,5 @@ def exists_word(word: str, instance: Queue):
     return occurrences_files
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue):
+    return exists_word(word, instance, True)
